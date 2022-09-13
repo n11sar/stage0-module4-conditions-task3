bd2fa9264ba98945bdcb584e7dac28f2feadfc0c
georgy <ganus1111@gmail.com>
2022-09-13 19:56:17 +0300

    add task solution

--- src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task3;
 
 public class AliquotNumbers {
     public void isFirstAliquot(int first, int second) {
+        if (first % second == 0){
+            System.out.println("Not aliquot");
+        }
+        if (first % second !=0){
+            System.out.println("Aliquot");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if ((character >= 'a' && character <= 'z') || (character >= 'A' && character <= 'Z')) {
+            if (character == 'a' || character == 'o' || character == 'e' || character == 'i' || character == 'u'
+            || character == 'A' || character == 'O' || character == 'E' || character == 'I' || character == 'U'){
+                System.out.println("Vowel");
+            }else {
+                System.out.println("Consonant");
+            }
+        }else {
+            System.out.println("wrong alphabet!");
+        }
     }
 }
